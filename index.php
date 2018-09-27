cc852b4db24230bed1c6373ded5e059c6dd2ddee
Luis Amigo <lamigo@praderas.org>
2018-09-27 16:18:49 +0200

    re-arranged requires on index

--- index.php
+++ index.php
@@ -2,37 +2,96 @@
 namespace Tuqan;
 
 include 'include.php';
+/**
+ * PEAR Files
+ */
 require_once 'PEAR.php';
 require_once 'vendor/autoload.php';
 require_once 'DB.php';
 require_once 'DB/pgsql.php';
 require_once 'HTML/Page.php';
-require_once 'Classes/FormatoPagina.php';
-require_once 'Classes/Manejador_De_Peticiones.php';
-require_once 'Classes/Manejador_Ayuda.php';
+require_once 'HTML/TreeMenu.php';
+require_once 'HTML/Table.php';
+require_once 'Pager/Pager.php';
+
+
+/**
+ * Forms
+ */
+require_once 'Classes/generador_Formularios.php';
+require_once 'Classes/Form_Administracion.php';
+require_once 'Classes/Form_Comun.php';
+require_once 'Classes/Form_Calidad.php';
+require_once 'Classes/Form_Medio.php';
+require_once 'Classes/Form_Nuevo.php';
+require_once 'Classes/forms.php';
+
+
+/**
+ * Old Generators
+ */
+require_once 'Classes/generador_SQL.php';
+require_once 'Classes/generador_arboles.php';
+require_once 'Classes/generador_listados.php';
+
+
+/**
+ * New Tuqan Classes
+ */
 require_once 'Classes/TuqanLogger.php';
-require_once 'Classes/Manejador_Editor.php';
-require_once 'Classes/Manejador_Funciones_Comunes.php';
-require_once 'Classes/Manejador_Formularios.php';
-require_once 'Classes/Manejador_Base_Datos.class.php';
-require_once 'Classes/Manejador_De_Respuestas.php';
-require_once 'Classes/Manejador_Listados.php';
+require_once 'Classes/Auth.php';
+require_once 'Classes/User.php';
+require_once 'Classes/AjaxHandler.php';
+
+/**
+ * Misc
+ */
+require_once 'Classes/FormatoPagina.php';
 require_once 'Classes/FormularioIdentificacion.php';
 require_once 'Classes/generador_SQL.php';
 require_once 'Classes/Config.php';
 require_once 'encriptador.php';
 require_once 'constantes.inc.php';
 require_once 'estilo.php';
+require_once 'Classes/Titulos.php';
+require_once 'boton.php';
+
+
+/**
+ * Old Handlers
+ */
+require_once 'Classes/Manejador_De_Peticiones.php';
+require_once 'Classes/Manejador_Ayuda.php';
+require_once 'Classes/Manejador_Editor.php';
+require_once 'Classes/Manejador_Funciones_Comunes.php';
+require_once 'Classes/Manejador_Formularios.php';
+require_once 'Classes/Manejador_Base_Datos.class.php';
+require_once 'Classes/Manejador_De_Respuestas.php';
+require_once 'Classes/Manejador_Listados.php';
+require_once 'Classes/Manejador_Detalles.php';
+
+
+/**
+ * Old Processors
+ */
 require_once 'Classes/Procesador_De_Peticiones.php';
 require_once 'Classes/Procesar_Listados.php';
+require_once 'Procesar_Editor.php';
+require_once 'Classes/Procesar_Funciones_Comunes.php';
+require_once 'Classes/Procesar_Detalles.php';
+require_once 'Classes/Procesar_Formularios.php';
+require_once 'Classes/Procesar_Arbol.php';
+require_once 'Classes/Procesar_Ayuda.php';
+
+
+/**
+ * New Pages
+ */
 require_once 'Pages/LoginEmpresa.php';
 require_once 'Pages/LoginUsuario.php';
 require_once 'Pages/IndexPage.php';
 require_once 'Pages/MainPage.php';
 require_once 'Pages/NotFoundPage.php';
-require_once 'Classes/Auth.php';
-require_once 'Classes/User.php';
-require_once 'Classes/AjaxHandler.php';
 
 use Phroute\Phroute\RouteCollector;
 use Phroute\Phroute\Dispatcher;
